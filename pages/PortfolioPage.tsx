8671ea7c848a122cec29b8260619c5bf6197637c
umang1528 <hellohaji665@gmail.com>
2025-08-12 18:17:11 +0530

    add

--- pages/PortfolioPage.tsx
+++ pages/PortfolioPage.tsx
@@ -133,6 +133,7 @@ export default function PortfolioPage() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {portfolioProjects.map((project) => (
             <PortfolioCard key={project.id} project={project} />
+            
           ))}
         </div>
       </div>
